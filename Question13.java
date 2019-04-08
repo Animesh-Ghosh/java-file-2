e0e78d0058dbc9425db3c9fa20f2887b6d2a0669
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-04-08 21:21:24 +0530

    Create Question13.java

---
+++ Question13.java
@@ -0,0 +1,13 @@
+import java.applet.*;
+import java.awt.event.*;
+import java.awt.*;
+
+/* program to accept details of employee(name, eno, mobile no., designation, salary) 
+	using AWT controls */
+
+/* <applet code="Question13" height="200" width="250">
+	</applet> */
+
+public class Question13 {
+	
+}
